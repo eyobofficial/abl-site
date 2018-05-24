d6ac63e0e3df873f4ecd2c2ba69e5b9f780570b6
Eyob Tariku <eyobtariku@gmail.com>
2018-05-24 14:39:59 +0300

    Create baset.html template

--- pages/views.py
+++ pages/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views.generic import TemplateView
 
-# Create your views here.
+
+class IndexView(TemplateView):
+    template_name = 'pages/index.html'
+
