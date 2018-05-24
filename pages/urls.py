5aeb59204507c68dd68bdbbf1beefd9f42ecba25
Eyob Tariku <eyobtariku@gmail.com>
2018-05-24 19:08:51 +0300

    create proposal templates

--- pages/urls.py
+++ pages/urls.py
@@ -7,4 +7,8 @@ app_name = 'pages'
 
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
+    path('p-1/', views.HomeView1.as_view(), name='p1-home'),
+    path('p-1/about/', views.AboutView1.as_view(), name='p1-about'),
+    path('p-1/service/', views.ServiceView1.as_view(), name='p1-service'),
+    path('p-1/contact/', views.ContactView1.as_view(), name='p1-contact')
 ]
--- pages/views.py
+++ pages/views.py
@@ -5,3 +5,38 @@ from django.views.generic import TemplateView
 class IndexView(TemplateView):
     template_name = 'pages/index.html'
 
+
+class HomeView1(TemplateView):
+    template_name = 'p1/home.html'
+
+    def get_context_data(self, *args, **kwargs):
+        context = super().get_context_data(*args, **kwargs)
+        context['page'] = 'home'
+        return context
+
+
+class AboutView1(TemplateView):
+    template_name = 'p1/about.html'
+
+    def get_context_data(self, *args, **kwargs):
+        context = super().get_context_data(*args, **kwargs)
+        context['page'] = 'about'
+        return context
+
+
+class ServiceView1(TemplateView):
+    template_name = 'p1/service.html'
+
+    def get_context_data(self, *args, **kwargs):
+        context = super().get_context_data(*args, **kwargs)
+        context['page'] = 'service'
+        return context
+
+
+class ContactView1(TemplateView):
+    template_name = 'p1/contact.html'
+
+    def get_context_data(self, *args, **kwargs):
+        context = super().get_context_data(*args, **kwargs)
+        context['page'] = 'contact'
+        return context
