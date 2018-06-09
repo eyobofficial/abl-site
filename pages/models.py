bd4a82e91e1121179a7c3b9461a01ff448c3c6ce
eyob tariku <eyobtariku@gmail.com>
2018-06-09 16:09:58 +0300

    Create abstract Base model

--- pages/models.py
+++ pages/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+
+class Base(models.Model):
+    """
+    Abstract Base Model
+    """
+    created_at = models.DateTimeField(
+        'Created Date',
+        auto_now_add=True,
+        help_text='Record first created date and time'
+    )
+    updated_at = models.DateTimeField(
+        'Last Modified',
+        auto_now=True,
+        help_text='Record last modified date and time'
+    )
+
+    class Meta:
+        abstract = True
+
